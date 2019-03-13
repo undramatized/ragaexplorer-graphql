5b7506719424e8ed3a7dbdd0b039fbc9509480b0
undramatized <siddarth21@hotmail.com>
2019-03-13 13:15:17 +0800

    update server listening port for heroku

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ const server = new ApolloServer({
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
